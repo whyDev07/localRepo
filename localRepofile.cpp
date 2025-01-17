567b736ab25ac1e95d0a890d3bdc820f487c4f69
Dev Adhikari <devadhikari5665@gmail.com>
2025-01-17 13:26:44 +0530

    adding something diff. for conflicts

--- localRepofile.cpp
+++ localRepofile.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(){
 cout<<"heeloo world";
-cout<<"MAKING THE CHANGES WITH THE GIT BRANCH FEATURE1";
+cout<<"MAKING THE CHANGES WITH THE GIT BRANCH FEATURE1(adding some thingelse for checking conflicts)";
 
 return 0;
 }
